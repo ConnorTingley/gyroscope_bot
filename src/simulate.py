033eebabdee87dd5a3d0e34c2d0ca472d1fc1f3c
ctingley <connoson@gmail.com>
2020-05-20 18:01:38 -0700

    first

---
+++ src/simulate.py
@@ -0,0 +1,19 @@
+import numpy as np
+class state:
+    mass = 0
+    length = 0
+    position = np.empty([3])
+    omega = np.empty([3])
+    wheel_ws = np.empty([3])
+    I = 0
+    def __init__(self, envelope, mass = 10, length = 1,):
+        self.mass = mass
+        self.length = length
+        self.I = mass * length * length
+        self.theta = np.empty([3])
+        self.omega = np.empty([3])
+        self.envelope = envelope
+        self.H = np.empty([3])
+
+    def simulate_step(self, dt):
+        alpha =
