33117e657430711444109d7f01dec22c0419914c
ctingley <connoson@gmail.com>
2020-06-10 16:06:53 -0700

    gym

--- src/gym.py
+++ src/gym.py
@@ -3,6 +3,7 @@ from gym import spaces
 from gym.utils import seeding
 import numpy as np
 from os import path
+from simulate import state
 
 
 class PendulumEnv(gym.Env):
@@ -11,26 +12,35 @@ class PendulumEnv(gym.Env):
         'video.frames_per_second': 30
     }
 
-    def __init__(self, g=10.0):
-        self.max_speed = 8
+    def __init__(self):
+        self.dt = .0003
+        self.step_count = 300
+        self.max_angular_vel = 1
         self.max_torque = 1.
-        self.dt = .05
-        self.g = g
-        self.m = 1.
-        self.l = 1.
+        self.max_flywheel = 1.
+        self.g = 9.8
+        self.mass = 10
+        self.length = 1.
+        self.L2 = 0.25
+        self.death_pos = np.pi/2
         self.viewer = None
 
-        high = np.array([1., 1., self.max_speed], dtype=np.float32)
+        self.sim = state(mass=self.mass, length=self.length, g=self.g, L2=self.L2,
+                    events=[[1, np.array([0, 0, 0]), np.array([1, 1, 1]), np.array([1, 1, 1])]],
+                    max_flywheel_l=np.array([self.max_flywheel, self.max_flywheel, self.max_flywheel]), max_torque=np.array([self.max_torque, self.max_torque, self.max_torque]))
+        self.state = self.sim.rough_step(self.dt, [0,0,0], 0)
+
         self.action_space = spaces.Box(
-            low=np.array([-self.max_torque, -self.max_torque, -self.max_torque]),
-            high=np.array([self.max_torque, self.max_torque, self.max_torque]),
+            low=np.array([-1, -1, -1]),
+            high=np.array([1, 1, 1]),
             shape=(3,),
             dtype=np.float32
         )
+        #Observation Space formatting: [phi, sin(alpha), cos(alpha), facing, d_phi, d_alpha, d_facing, wheel_l1, wheel_l2, wheel_l3]
         self.observation_space = spaces.Box(
-            low=-high,
-            high=high,
-            shape=(3,),
+            low=np.array([0, -1., -1., , -self.max_angular_vel, -self.max_angular_vel, -self.max_angular_vel, -1, -1, -1], dtype=np.float32),
+            high=np.array([self.death_pos, 1., 1., , self.max_angular_vel, self.max_angular_vel, self.max_angular_vel, 1, 1, 1], dtype=np.float32),
+            shape=(9,),
             dtype=np.float32
         )
 
@@ -41,56 +51,49 @@ class PendulumEnv(gym.Env):
         return [seed]
 
     def step(self, u):
-        th, thdot = self.state  # th := theta
+        self.state = self.sim.rough_step(self.dt, u, self.step_count)
 
-        g = self.g
-        m = self.m
-        l = self.l
-        dt = self.dt
 
-        u = np.clip(u, -self.max_torque, self.max_torque)[0]
-        self.last_u = u  # for rendering
-        costs = angle_normalize(th) ** 2 + .1 * thdot ** 2 + .001 * (u ** 2)
-
-        newthdot = thdot + (-3 * g / (2 * l) * np.sin(th + np.pi) + 3. / (m * l ** 2) * u) * dt
-        newth = th + newthdot * dt
-        newthdot = np.clip(newthdot, -self.max_speed, self.max_speed)
-
-        self.state = np.array([newth, newthdot])
         return self._get_obs(), -costs, False, {}
 
     def reset(self):
-        high = np.array([np.pi, 1])
-        self.state = self.np_random.uniform(low=-high, high=high)
-        self.last_u = None
+        self.state = self.sim.reset()
         return self._get_obs()
 
     def _get_obs(self):
-        theta, thetadot = self.state
-        return np.array([np.cos(theta), np.sin(theta), thetadot])
+        t, angle, angle_vel, wheel_l = self.state
+        theta, phi, alpha, facing = angle
+        d_theta, d_alpha, d_phi, d_facing = angle_vel
+
+        return np.array([phi, np.sin(alpha), np.cos(alpha), facing, d_phi, d_alpha, d_facing, wheel_l[0], wheel_l[1], wheel_l[2]])
 
     def render(self, mode='human'):
         if self.viewer is None:
             from gym.envs.classic_control import rendering
             self.viewer = rendering.Viewer(500, 500)
             self.viewer.set_bounds(-2.2, 2.2, -2.2, 2.2)
-            rod = rendering.make_capsule(1, .2)
-            rod.set_color(.8, .3, .3)
+
+            rod = rendering.Line((0,0), (0,1))
+            rod.set_color(0., 0., 0.)
+
             self.pole_transform = rendering.Transform()
             rod.add_attr(self.pole_transform)
             self.viewer.add_geom(rod)
-            axle = rendering.make_circle(.05)
-            axle.set_color(0, 0, 0)
-            self.viewer.add_geom(axle)
-            fname = path.join(path.dirname(__file__), "assets/clockwise.png")
-            self.img = rendering.Image(fname, 1., 1.)
-            self.imgtrans = rendering.Transform()
-            self.img.add_attr(self.imgtrans)
+
+            tip = rendering.make_circle(.1)
+            tip.set_color(0.2, 0.2, 0.9)
+            self.tip_transform = rendering.Transform()
+            rod.add_attr(self.tip_transform)
+            self.viewer.add_geom(tip)
+
 
         self.viewer.add_onetime(self.img)
-        self.pole_transform.set_rotation(self.state[0] + np.pi / 2)
-        if self.last_u:
-            self.imgtrans.scale = (-self.last_u / 2, np.abs(self.last_u) / 2)
+        angle = self.state[0]
+        radius = np.sin(angle[1])
+        self.pole_transform.set_rotation(angle[0])
+        self.pole_transform.set_scale(1,radius)
+
+        self.tip_transform.set_translation(radius * np.cos(angle),radius * np.sin(angle))
 
         return self.viewer.render(return_rgb_array=mode == 'rgb_array')
 
