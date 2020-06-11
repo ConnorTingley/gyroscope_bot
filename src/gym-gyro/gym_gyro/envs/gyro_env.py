a95f30651b3152d64a13e00d781b0f465f26acd6
ctingley <connoson@gmail.com>
2020-06-10 18:08:23 -0700

    venv setting

--- src/gym-gyro/gym_gyro/envs/gyro_env.py
+++ src/gym-gyro/gym_gyro/envs/gyro_env.py
@@ -1,6 +1,106 @@
 from __future__ import division
+import gym
+from gym import spaces
+from gym.utils import seeding
 import numpy as np
+from os import path
 import math
+
+
+class GyroEnv(gym.Env):
+    metadata = {
+        'render.modes': ['human'],
+        'video.frames_per_second': 30
+    }
+
+    def __init__(self):
+        self.dt = .0003
+        self.step_count = 300
+        self.max_angular_vel = 1
+        self.max_torque = 1.
+        self.max_flywheel = 1.
+        self.g = 9.8
+        self.mass = 10
+        self.length = 1.
+        self.L2 = 0.25
+        self.death_pos = np.pi/2
+        self.viewer = None
+
+        self.sim = state(mass=self.mass, length=self.length, g=self.g, L2=self.L2,
+                    events=[[1, np.array([0, 0, 0]), np.array([1, 1, 1]), np.array([1, 1, 1])]],
+                    max_flywheel_l=np.array([self.max_flywheel, self.max_flywheel, self.max_flywheel]), max_torque=np.array([self.max_torque, self.max_torque, self.max_torque]))
+        self.state = self.sim.rough_step(self.dt, [0,0,0], 0)
+
+        self.action_space = spaces.Box(
+            low=np.array([-1, -1]),
+            high=np.array([1, 1]),
+            shape=(2,),
+            dtype=np.float32
+        )
+        #Observation Space formatting: [phi, sin(theta), cos(theta), d_phi, d_theta, wheel_l1, wheel_l2]
+        self.observation_space = spaces.Box(
+            low=np.array([0, -1., -1., -self.max_angular_vel, -self.max_angular_vel, -1, -1], dtype=np.float32),
+            high=np.array([self.death_pos, 1., 1., self.max_angular_vel, self.max_angular_vel, 1, 1], dtype=np.float32),
+            shape=(7,),
+            dtype=np.float32
+        )
+
+        self.seed()
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def step(self, u):
+        self.state, reward = self.sim.rough_step(self.dt, u, self.step_count)
+
+        return self._get_obs(), reward, False, {}
+
+    def reset(self):
+        self.state = self.sim.reset()
+        return self._get_obs()
+
+    def _get_obs(self):
+        t, angle, angle_vel, wheel_l = self.state
+        theta, phi, alpha, facing = angle
+        d_theta, d_phi, d_alpha, d_facing = angle_vel
+
+        return np.array([phi, np.sin(theta), np.cos(theta), d_phi, d_theta, wheel_l[0], wheel_l[1]])
+
+    def render(self, mode='human'):
+        if self.viewer is None:
+            from gym.envs.classic_control import rendering
+            self.viewer = rendering.Viewer(500, 500)
+            self.viewer.set_bounds(-2.2, 2.2, -2.2, 2.2)
+
+            rod = rendering.Line((0,0), (0,1))
+            rod.set_color(0., 0., 0.)
+
+            self.pole_transform = rendering.Transform()
+            rod.add_attr(self.pole_transform)
+            self.viewer.add_geom(rod)
+
+            tip = rendering.make_circle(.1)
+            tip.set_color(0.2, 0.2, 0.9)
+            self.tip_transform = rendering.Transform()
+            rod.add_attr(self.tip_transform)
+            self.viewer.add_geom(tip)
+
+
+        angle = self.state[1]
+        radius = np.sin(angle[1])
+        self.pole_transform.set_rotation(angle[0])
+        self.pole_transform.set_scale(1,radius)
+
+        self.tip_transform.set_translation(radius * np.cos(angle),radius * np.sin(angle))
+
+        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
+
+    def close(self):
+        if self.viewer:
+            self.viewer.close()
+            self.viewer = None
+
 
 class state:
     # physical parameters of system
@@ -114,11 +214,11 @@ class state:
         fractional_L = self.wheel_l / self.satr_l
         phi_dot_opt = - self.angle[1] / np.sqrt(self.ag)
         reward = - self.reward_consts[0] * np.cos(self.angle[1]) - \
-                 self.reward_consts[1] * fractional_L[1] ** 2 - self.reward_consts[2] (self.angle_vel[1] - phi_dot_opt)**2
+                 self.reward_consts[1] * fractional_L[1] ** 2 - self.reward_consts[2] * (self.angle_vel[1] - phi_dot_opt)**2
         return [self.t, self.angle, self.angle_vel, self.wheel_l/ self.satr_l], reward
 
     def reset(self):
-        self.angle = np.array([0.,np.random.triangular(.3, .2, .1),0.,0.])
+        self.angle = np.array([0.,np.random.triangular(.1, .2, .3),0.,0.])
         self.angle_vel = np.array([.0,0.,0.,0.])
         self.wheel_l = np.array([0.,0.,0.])
         self.t = 0
